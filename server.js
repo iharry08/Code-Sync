cace8c6849df4e78028849f10b48a38a9080409e
Hardik <hardik.82001@gmail.com>
2022-06-27 17:52:29 +0530

    Server changes

--- server.js
+++ server.js
@@ -63,5 +63,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
